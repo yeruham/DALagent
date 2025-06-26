bf4d74c810b23a33b274d819f3d59ae4adb01b17
YeruhamMendelson <y0583207549@gmail.com>
2025-06-26 11:47:30 +0300

    class of agent

--- models/agent.py
+++ models/agent.py
@@ -1,3 +1,9 @@
 class Agent:
 
-    pass
+    def __init__(self, code_name: str, real_name: str, location: str, status: str, missions_completed: int, id: int= 0):
+        self.code_name = code_name
+        self.real_name = real_name
+        self.location = location
+        self.status = status
+        self.missions_completed = missions_completed
+        self.id = id
